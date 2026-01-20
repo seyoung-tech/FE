342a7b123e814ab0011a7b65d872653416590780
gupal05 <dudgh2349@naver.com>
2026-01-20 15:13:08 +0900

    build error

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -147,7 +147,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, onUnmounted, computed } from "vue";
+import { ref, onMounted, onUnmounted } from "vue";
 import { RouterLink } from "vue-router";
 import { contactInfo } from "../config/contact";
 
